4961e6bd426c231821e4afc7a048219eb5804bc0
Derrick Mei <derrick.mei.168@gmail.com>
2020-03-16 22:23:43 -0500

    rock paper scissors

--- 4. rock_paper_scissors/rps.py
+++ 4. rock_paper_scissors/rps.py
@@ -1,14 +1,41 @@
 #!/usr/bin/python
 
 import sys
+"""
+base case - n == 1, return ['rock], ['paper], ['scissors]
+"""
+
 
 def rock_paper_scissors(n):
-  pass 
+    baseArr = [['rock'], ['paper'], ['scissors']]
+
+    if n == 0:
+        return [[]]
+
+    if n == 1:
+        return baseArr
+
+    while n > 1:
+        tempArr = [[]] * len(baseArr)*3
+        for i in range(len(baseArr)):
+            print(f'i: {i}')
+            rock = baseArr[i][:]
+            paper = baseArr[i][:]
+            scissors = baseArr[i][:]
+            rock.append('rock')
+            paper.append('paper')
+            scissors.append('scissors')
+            tempArr[i*3+0] = rock
+            tempArr[i*3+1] = paper
+            tempArr[i*3+2] = scissors
+        baseArr = tempArr
+        n -= 1
+    return baseArr
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
